2ca452c2101631bd0dcc1cab6402eed9add5ed89
Reixen <123600070+Reixen@users.noreply.github.com>
2025-07-15 21:29:27 -0700

    Transferred main.lua comments into own txt files

--- main.lua
+++ main.lua
@@ -61,30 +61,3 @@ for _, folder in ipairs(fileStructure) do
         end
     end
 end
-
-----------------------------------------------------------------
----                          TO DO:                          ---
-----------------------------------------------------------------
---- 1) Bugfixes - In progress!
---- 2) Sfx - Currently under review!
---- 
---- Future TODO:
----     - Lie Down
----        - When initialized, sleep
----        - Wake up and stretch if approached.
----        - Lie back to position if within range, otherwise stand.
----     - Bork Set 2
----     
---- Low Priority Bugs
----     - Get the pickup back when HG is used after leaving the room mid-fetch
---- 
---- Attribution:
---- yawn: https://freesound.org/people/jinxycat49/sounds/490164/
---- feeding_bowl_pour.wav: Pouring dog food into bowl by Ryntjie -- https://freesound.org/s/365052/ -- License: Attribution NonCommercial 3.0
---- feeding_bowl_fall.wav: Metal Dog Bowl Falling.wav by ChamoneSteyn -- https://freesound.org/s/542210/ -- License: Attribution 4.0
---- pant_1 and pant_2: dog panting + half growl + whine.WAV by pogmothoin -- https://freesound.org/s/401307/ -- License: Attribution 4.0
---- crunch and mini_crunch.wavs: Dog_Eat.wav by Blu_150058 -- https://freesound.org/s/326212/ -- License: Attribution NonCommercial 3.0
---- gulp.wav: Swallowing and gulping by 170084 -- https://freesound.org/s/408205/ -- License: Attribution NonCommercial 4.0
---- wall_hit_ding.wav: Bell Ding by Natty23 -- https://freesound.org/s/411749/ -- License: Attribution 4.0
---- Unused
---- Dog Sounds by iainmccurdy -- https://freesound.org/s/640743/ -- License: Attribution 4.0 -- Unused
